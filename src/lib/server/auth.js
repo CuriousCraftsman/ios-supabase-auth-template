89999da6221860be225d069f383ca4157f199298
SanjayS5 <sanjaysudhakaran@gmail.com>
2025-04-02 09:12:23 +1300

    cloudflare can't import env vars the SK way

--- src/lib/server/auth.js
+++ src/lib/server/auth.js
@@ -1,7 +1,9 @@
 import { betterAuth } from "better-auth";
 import pg from "pg"
 const { Pool } = pg;
-import { DATABASE_URL, BETTER_AUTH_SECRET } from '$env/static/private';
+
+const DATABASE_URL = import.meta.env.DATABASE_URL
+const BETTER_AUTH_SECRET = import.meta.env.BETTER_AUTH_SECRET
 
 export const auth = betterAuth({
     database: new Pool({
