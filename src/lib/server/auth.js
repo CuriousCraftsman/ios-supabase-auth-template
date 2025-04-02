17326a355b6b7090eed7b905c27d1a382d169149
SanjayS5 <sanjaysudhakaran@gmail.com>
2025-04-02 22:06:01 +1300

    google sign in support

--- src/lib/server/auth.js
+++ src/lib/server/auth.js
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import pg from "pg"
 const { Pool } = pg;
-import { DATABASE_URL, BETTER_AUTH_SECRET } from '$env/static/private';
+import { DATABASE_URL, BETTER_AUTH_SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
 
 export const auth = betterAuth({
     database: new Pool({
@@ -11,11 +11,10 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true
     },
-    // SEE THIS: https://www.better-auth.com/docs/authentication/google
-    // socialProviders: {
-    //     google: {
-    //         clientId: process.env.GOOGLE_CLIENT_ID,
-    //         clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    //     },
-    // },
+    socialProviders: {
+        google: {
+            clientId: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET
+        },
+    },
 });
